74eea6eda2cb739941bc3f9c708f5748fa7d2649
Pablosudev <pablo.losu.dev@gmail.com>
2024-11-11 09:47:20 +0100

    Botones cambio API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import"./app.css" ;
+import { CardsCity } from "./COMPONENTES/CardsComponentes/CardsCityComponents";
 import { NavBar } from './COMPONENTES/NAVBAR/Navbar'
 import { CitySearch } from "./COMPONENTES/SEARCHCITY/CitySearchComponent";
 
 
-function App() {
+
+export const App = () => {
   
+  const [apiData,setApiData] = useState(null)
+  const [apiUrlPage, setApiUrlPage] = useState("https://pokeapi.co/api/v2/location/")
+  const [nextUrlPage, setNextUrlPage] = useState(null)
+  const [backUrlPage, setBackUrlPage] = useState(null)
+
+  useEffect(() => {
+    fetch(apiUrlPage).then((response) => {
+        response.json().then((data) => {
+
+          setApiData(data)
+          setNextUrlPage(data.next)
+          setBackUrlPage(data.previous)
+        })
+    })
+},[apiUrlPage])
+
+
+  const NextPage = () => {
+    setApiUrlPage(nextUrlPage)
+  }
+
+  const BackPage = () => {
+    setApiUrlPage(backUrlPage)
+  }
 
   return (
    <>
@@ -12,12 +39,23 @@ function App() {
     <NavBar/>
     <section className ="header__section">
     <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="Pokeapi" />
-    <h2>LA NUEVA API DE POKéMON</h2>
-    <p>Descubre todos los datos del mundo Pokémon</p>
+    <h2 className="titledate">LA NUEVA API DE POKéMON</h2>
+    <p className="date">Descubre todos los datos del mundo Pokémon</p>
     </section>
    </header>
    <section>
-    <CitySearch/>
+    <CitySearch apiData={apiData}/>
+   </section>
+   <section>
+    <button onClick= {BackPage}>Anterior</button>
+    <button onClick={NextPage}>Siguiente</button>
+   </section>
+   <section>
+    {apiData !=null ? <> {
+      apiData.results.map((city,index) => {
+      return <CardsCity key={index} name={city.name} url={city.url}/>
+    })
+    } </> : <p>Loading...</p>}
    </section>
    </>
   )
